0c6fb60726eee3dc78559082005972848241b191
Busu <busurmanbek000@gmail.com>
2022-10-06 12:09:01 +0600

    New class Class1

---
+++ src/CLass1.java
@@ -0,0 +1,14 @@
+public class CLass1 {
+    String name;
+
+    public CLass1(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CLass1{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}

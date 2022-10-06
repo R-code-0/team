0350f007c4a0468abf70f76397df274a1b5956b3
User <mirgulabdinazarova@gmail.com>
2022-10-06 12:12:39 +0600

    commit 2

--- src/CLass1.java
+++ src/CLass1.java
@@ -5,6 +5,14 @@ public class CLass1 {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "CLass1{" +
